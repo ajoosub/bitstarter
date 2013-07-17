5d2d2c9ea20017354d12efe4cc9a0f5b03a69487
Ahmed Joosub <ajoosub0815@gmail.com>
2013-07-17 20:03:21 +0000

    new web.js

--- web.js
+++ web.js
@@ -1,13 +1,12 @@
 var express = require('express');
-//var fs = require('fs');
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
-app.get('/index.html', function(request, response) {
+app.get('/', function(request, response) {
 
-app.use(express.static('/index.html' + '/bitstarter'));
-//var content = fs.readFileSync('./index.html','utf8');
-  //response.render(content);
-});
+var content = fs.readFileSync('index.html','utf8');
+  response.end(content.toString());
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
