9738205f29e86c42a645a44e516e105bf06e931f
Ahmed Joosub <ajoosub0815@gmail.com>
2013-07-17 17:01:29 +0000

    newUpdated2 web.js

--- web.js
+++ web.js
@@ -2,9 +2,9 @@ var express = require('express');
 vat fs = require('fs');
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
+app.get('index.html', function(request, response) {
 var content = fs.readFileSync('index.html','utf8');
-  response.send(content);
+  response.end(content.toString());
 });
 
 var port = process.env.PORT || 5000;
