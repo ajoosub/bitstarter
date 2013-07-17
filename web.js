bc6db2252fb8622b46b5c4cf495f754022ed4c76
Ahmed Joosub <ajoosub0815@gmail.com>
2013-07-17 04:24:03 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,16 +1,30 @@
-var express=require('express');
+var fs = require('fs');
 
+//var goodWords = "Yet today I consider myself the luckiest man on the face of this earth.";
 
-var app = express.createServer(express.logger());
+//fs.writeFileSync('lou.txt', someText);
 
-app.use(router);
-app.use(express.static(index.html + "/"));
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+fs.readFile('index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(data);
 });
+//var http://
+//var express=require('express');
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+
+//var app = express.createServer(express.logger());
+
+//app.use(router);
+//app.use(express.static(index.html + "/"));//
+//app.get('/', function(request, response) {
+  //response.send('Hello World 2!');
+//});
+
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+//  console.log("Listening on " + port);
+//});
 
