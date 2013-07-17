0be7b681b2f727a4551a46c8ae23a92eb8cb3cc4
Ahmed Joosub <ajoosub0815@gmail.com>
2013-07-17 04:13:00 +0000

    file Info been taken

--- web.js
+++ web.js
@@ -1,7 +1,10 @@
-var express = require('express');
+var express=require('express');
+
 
 var app = express.createServer(express.logger());
 
+app.use(router);
+app.use(express.static(index.html + "/"));
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
@@ -10,3 +13,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
